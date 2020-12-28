5629c9c5ace4e316070a810ea2b07c0ad42d0be7
GamePlayer-PL <gameplayer2019pl@gmail.com>
2020-12-28 10:33:46 +0100

    EssentialsGP plugin 0.1 DEV un-workable

---
+++ src/io/github/gameplayerpl/Main.java
@@ -0,0 +1,13 @@
+package io.github.gameplayerpl;
+
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        getLogger().info("EssentialsGP starting...");
+        getLogger().info("Checking libraries:");
+
+    }
+}
